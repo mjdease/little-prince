eeced1e04c7ff5736bd96112eb9789f56d71d0c8
Matt Dease <mattdease+dev@gmail.com>
2014-02-07 12:50:58 -0500

    earthIntro6 idle animations.

--- js/story/earthIntro-06.js
+++ js/story/earthIntro-06.js
@@ -1,3 +1,28 @@
 var Page = require("./page");
+var util = require("../util");
 
-module.exports = new Page("earthIntro", 6, false);
+var page = new Page("earthIntro", 6, false);
+
+var hand;
+
+page.setRequiredAssets([
+    {name: "hand", file: "hand.png"}
+]);
+
+page.initPage = function(images, stage, layers){
+    hand = util.defineSprite({
+        x:0,
+        y:330,
+        image: images.hand,
+        animation: "idle",
+        frameRate: 7
+    }, 1223, 607, [{idle: 3}]);
+
+    layers.dynBack.add(hand);
+};
+
+page.startPage = function(){
+    hand.start();
+};
+
+module.exports = page;
